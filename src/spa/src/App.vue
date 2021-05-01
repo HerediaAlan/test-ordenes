4fe287ac765126f225e146c964ab4f1ca63d4c7a
Alan G. Heredia <hgarcia.alan@gmail.com>
2021-05-01 15:12:15 -0700

    Dashboard: Mostrar información sobre clientes

--- src/spa/src/App.vue
+++ src/spa/src/App.vue
@@ -14,6 +14,11 @@ export default {
     name: "App",
     components: {
         Menu
+    },
+    watch: {
+        '$route' (to) {
+            document.title = to.meta.title
+        }
     }
 };
 </script>
--- src/spa/src/components/Home.vue
+++ src/spa/src/components/Home.vue
@@ -1,13 +1,22 @@
 <template>
     <div id="contenedor">
         <div id="info-cliente">
-            Work in Progress
+            <h2>Información sobre clientes</h2>
+
+            <span class="font-weight-bold">Cantidad de clientes existentes:</span>
+            <p>{{ this.clientes.length }}</p>
+
+            <span class="font-weight-bold">Top 10 clientes con mayor cantidad de ordenes de soporte:</span>
+            <b-table :items="ordenarClientes" :fields="clientesColumnas" small></b-table>
+
+            <span class="font-weight-bold">Cantidad de clientes por entidad federativa:</span>
+            <b-table :items="filtrarPorEntidad" :fields="clientesColumnasEntidad" small></b-table>
         </div>
         <div id="info-ordenes"></div>
     </div>
 </template>
 
-<script>
+<script> 
 const axios = require("axios");
 
 export default {
@@ -15,26 +24,69 @@ export default {
     data() {
         return {
             clientes: {},
+            clientesColumnas: ["nombre", "primerApellido", "segundoApellido", "ciudad", "cantidad"],
+            clientesColumnasEntidad: ["entidadFederativa", "cantidad"],
             ordenes: {}
         }
     },
+    computed: {
+        ordenarClientes() {
+            let datos = this.clientes
+            let orden = datos.sort((a, b) => b.Ordenes.length - a.Ordenes.length).slice(0, 10)
+            return orden
+        },
+        filtrarPorEntidad() {
+            let datos = this.clientes
+            const result = [...datos.reduce((r, o) => {
+                const key = o.entidadFederativa;
+                
+                const item = r.get(key) || Object.assign({}, o, {
+                    entidadFederativa: "",
+                    cantidad: 0
+                });
+                
+                item.entidadFederativa = o.entidadFederativa;
+                item.cantidad += o.entidadFederativa === item.entidadFederativa ? 1 : 0;
+
+                return r.set(key, item);
+                }, new Map).values()];
+            
+            return result.sort((a, b) => b.cantidad - a.cantidad)
+        }
+    },
+    beforeMount() {
+        this.obtenerDatos();
+        this.obtenerOrdenes();
+    },
     methods: {
+        groupBy(datos) {
+            const entidades = datos.map(x => x.entidadFederativa);
+            return entidades.unique();
+        },
         obtenerDatos() {
             axios
                 .get("http://localhost:10040/clientes")
                 .then(response => {
                     const dataClientes = response.data.data
+                    dataClientes.forEach(element => {
+                        element.cantidad = element.Ordenes.length
+                    });
                     this.clientes = dataClientes
                 })
+                .catch((error) => console.log(error.response.data));
+        },
+        obtenerOrdenes() {
+            axios
+                .get("http://localhost:10040/ordenes")
+                .then((response) => {
+                    const dataOrdenes = response.data.data;
+                    this.ordenes = dataOrdenes;
+                })
+                .catch((error) => console.log(error.response.data));
         }
     }
 }
 </script>
 
 <style>
-#contenedor {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-}
 </style>
